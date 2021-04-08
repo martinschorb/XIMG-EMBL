2861b1af3d23ec5650522a26e7fa1eee0a6ba368
Martin Schorb <martin.schorb@embl.de>
2021-04-08 10:27:55 +0200

    dask client rewrite

--- dask_client_rewrite.py
+++ dask_client_rewrite.py
@@ -1,45 +1,61 @@
-import matplotlib
-matplotlib.use('Agg')
-import sys, time
-import dxchange, tomopy
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  7 09:18:47 2021
+
+@author: schorb
+"""
+
+
+
 import numpy as np
-# import tifffile
-from scipy.ndimage import rotate
-# from contextlib import closing
-# from multiprocessing import Pool
-import gc
-from maximus48 import monochromaticCTF as CTF 
+import os
+import sys
+
+
 
 from maximus48 import var
+from maximus48.tomo_proc3 import init_Npad, init_names_custom, F
 from maximus48 import SSIM_131119 as SSIM_sf 
+from maximus48.SSIM_131119 import SSIM_const
 from maximus48 import multiCTF2 as multiCTF
-from maximus48.SSIM_131119 import SSIM_const 
-from maximus48.tomo_proc3 import (Processor, F, tonumpyarray, rotscan)
-from maximus48 import FSC
+
+
+from skimage.io import imread, imsave
 
 # from pybdv import make_bdv 
 
-
-import dask
-import dask.array as da
-from dask.array.image import imread
+# import dask
+# import dask.array as da
+from skimage.io import imread,imsave
 
 from dask.distributed import Client, progress
 
 from dask_jobqueue import SLURMCluster
-import os
 
 
 
 
-#import matplotlib.pyplot as plt
-#from maximus48 import monochromaticCTF as CTF 
-#import h5py
-#from maximus48.tomo_proc3 import axis_raws, interpolate
+#%% 
+
+
+#-----------------------------------------
+
+
+#         DASK 
+
+# ----------------------------------------
+
+
+
+
+client = Client('10.11.12.87:34117')
+
+#%%
 
 
 # =============================================================================
-#           parameters for phase retrieval with CTF
+#           initial parameters for phase retrieval with CTF
 # =============================================================================
 N_steps = 10                                                                   # Number of projections per degree
 N_start = 1                                                                    # index of the first file
@@ -60,108 +76,120 @@ inclination = -0.23                                                            #
 
 #data_name = 'ew21_5'
 data_name = 'Platy-12601'
-folder = '/scratch/schorb/HH_platy/raw/'
-folder_result = '/g/emcf/schorb/data/HH_platy/rec'
+folder_base = '/g/emcf/schorb/data/HH_platy'
+folder = os.path.join(folder_base,'raw')
+folder_temp = os.path.join(folder_base,'tmp')
+folder_result = os.path.join(folder_base,'rec')
 distances = (1,2,3,4)
-N_distances  = 4                                                               # number of distances in phase-retrieval
+N_distances  = 4  
 
-
-# =============================================================================
-#    prepartion work 
-# =============================================================================
-print('\n##################################################################\n',
-      data_name, "started with %d cpus on" % cp_count, time.ctime(),
-      '\n##################################################################\n')
-time1 = time.time()
 
 #calculate parameters for phase-retrieval
 wavelength = var.wavelen(energy)
 fresnelN = pixel**2/(wavelength*distance)
 
+
 #create save folder if it doesn't exist
+
+if not os.path.exists(folder_temp):
+    os.makedirs(folder_temp)
+
+
 if not os.path.exists(folder_result):
     os.makedirs(folder_result)
 
 
-
-#%% 
-
-
-#-----------------------------------------
-
-
-#         DASK 
-
-# ----------------------------------------
-
-
-
-
-client = Client('10.11.12.87:34117')
+# Variable structure:
+            # ['N_files',
+            #  'N_start',
+            #  'Npad',
+            #  'ROI',
+            #  'ROI_ff',            
+            #  'flats',
+            #  'im_shape',
+            #  'images',
+            #  'init_paths']
+            
+            
+Npad = init_Npad(ROI, compression = 8)
 
 
+#%%
 
-# create a class to store all necessary parameters for parallelization
-Pro = Processor(ROI, folder, N_start, N_finish, compNpad = 8)                 
+# Support functions
+# =============================
 
-#set proper paths
-Pro.init_paths(data_name, folder, distances) 
+def init_paths(data_name, path, distance_indexes):
+    """Generate paths images & flatfields"""
 
-#allocate memory to store flatfield
-shape_ff = (N_distances, len(Pro.flats[0]), Pro.im_shape[0], Pro.im_shape[1]) 
-# ff_shared = F(shape = shape_ff, dtype = 'd')
-
-
-
-
-#read ff-files to memory
-
-ff_l =[]
-
-for i in range(N_distances):
-    fname = Pro.flats[i][0].rsplit('_00')[0]
-    ff_l.append(imread(fname+'*')[:,ROI[1]:ROI[3], ROI[0]:ROI[2]])
+    #set data_names
+    data_names, ff_names = init_names_custom(data_name = data_name,
+                                             distance_indexes = distance_indexes)
     
-ff = da.stack(ff_l)
+    #find images
+    imlist = var.im_folder(path)
+    
+    # create a filter for unnecessary images
+    tail = '_00000.tiff'
+    
+    if len(data_names[0])!=len(data_names[-1]):
+        print("""
+        WARNING! Different distances in your dataset 
+        have different naming lengths. 
+        File names can't be arranged. 
+        Try to reduce the number of distances (<10) or modify the script.
+        """)
+        sys.exit()
+    else:
+        data_lencheck = len(data_names[0]+tail)
+        ff_lencheck = len(ff_names[0]+tail)
+    
+
+    
+    #set proper paths
+    N_distances = len(distance_indexes) 
+    images = np.zeros(N_distances, 'object') 
+    flats = np.zeros(N_distances, 'object')
+            
+    for i in np.arange(len(images)):
+        
+        #sort image paths
+        images[i] = [path+im for im in imlist 
+                     if (im.startswith(data_names[i])) 
+                     and not (im.startswith('.'))
+                     and (len(im) == data_lencheck)]
+        
+        flats[i] = [path+im for im in imlist 
+                    if im.startswith(ff_names[i])
+                    and (len(im)==ff_lencheck)]
+
+    return images,flats
+
+
+# =============================
+
+
+## FLAT field correction
+
+def flat_correct(j,images=images,flats=flats,distances=distances,ff_con=ff_con):
+    filt = []
+    for i in np.arange(len(images)):
+        im = imread(images[i][j])[ROI[1]:ROI[3], ROI[0]:ROI[2]]
+        maxcorridx=np.argmax(SSIM_sf.SSIM(SSIM_const(im[ROI_ff[1]:ROI_ff[3], ROI_ff[0]:ROI_ff[2]]), 
+                                        ff_con[i]).ssim())
+        
+        filt.append(im/ff[i][maxcorridx])
+    
+    return filt
+
+# =============================
+
+
+## holographic reconstruction
 
 
 
-#calculate ff-related constants
-Pro.ROI_ff = (ff.shape[3]//4, ff.shape[2]//4,3 * ff.shape[3]//4, 3 * ff.shape[2]//4)    # make ROI for further flatfield and shift corrections, same logic as for normal ROI
-ff_con = np.zeros(N_distances, 'object')                                                # array of classes to store flatfield-related constants
-for i in np.arange(N_distances):    
-    ff_con[i] = SSIM_const(ff[i][:,Pro.ROI_ff[1]:Pro.ROI_ff[3], 
-                                   Pro.ROI_ff[0]:Pro.ROI_ff[2]].transpose(1,2,0))
-
-
-
-# #allocate memory to store ff-indexes
-# indexes = F(shape = (N_finish - N_start, N_distances), dtype = 'i' )
-
-# #allocate memory to store shifts
-# shifts = F(shape = (N_finish - N_start, N_distances, 2), dtype = 'd')
-
-# #allocate memory to store filtered files
-# proj = F(shape = (Pro.N_files, shape_ff[2], shape_ff[3] + 2*Pro.Npad), dtype = 'd' )
-
-# #print('finished calculation of ff-constants and memory allocation in ', time.time()-time1)
-
-
-
-# # =============================================================================
-# # =============================================================================
-# # # Processing module
-# # =============================================================================
-# # =============================================================================
-
-# # =============================================================================
-# # functions for parallel processing 
-# # =============================================================================
-
-# def init():
-#     global Pro, ff_shared, ff_con, proj
-
-def read_flat(j,Pro=Pro): 
+def read_flat(j, images=images, ROI_ff=ROI_ff, ROI=ROI,flats=flats,distances=distances,ff_con=ff_con, N_start=N_start, Npad=Npad): 
     
     """
     j: int
@@ -169,31 +197,9 @@ def read_flat(j,Pro=Pro):
     Please note, j always starts from zero
     To open correct file, images array uses images[i][j + N_start-1]
     """
-
-    
-    ROI_ff = Pro.ROI_ff
-    ROI = Pro.ROI
-    images = Pro.images
-    N_start = Pro.N_start
-    Npad = Pro.Npad
      
     #read image and do ff-retrieval    
-    filt = []
-        
-    imnames = images[0][j].partition('_'+str(distances[0])+'_')[0]+'_*_'+'{:05d}'.format(1)+'.'+images[0][j].partition('.')[-1]
-    
-    im = imread(imnames)[:,ROI[1]:ROI[3], ROI[0]:ROI[2]]
-    maxcorridx = []
-    
-    filts = []
-    
-    for i in np.arange(len(images)):        
-        maxcorridx=np.argmax(SSIM_sf.SSIM(SSIM_const(im[i][ROI_ff[1]:ROI_ff[3], ROI_ff[0]:ROI_ff[2]]), 
-                                        ff_con[i]).ssim())
-        
-        filts.append(im[i]/ff[i][maxcorridx])
-    
-    filt = da.stack(filts)
+    filt = flat_correct(j,images=images,flats=flats,distances=distances,ff_con=ff_con)
     
     im_gau0 = var.filt_gauss_laplace(filt[0][ROI_ff[1]:ROI_ff[3], ROI_ff[0]:ROI_ff[2]],
                                     sigma = 5)
@@ -212,25 +218,53 @@ def read_flat(j,Pro=Pro):
                                           fresnelN, zero_compensation)
     filt0 = filt0[Npad:(filt0.shape[0]-Npad),:]
 
+    imsave(os.path.join(folder_temp,''.join(os.path.basename(images[0][j]).partition('_'+str(distances[0]))[0:3:2])))
+    # pda = da.from_array(filt0)
+    # da.to_zarr(pda,'/scratch/schorb/HH_platy/Platy-12601_'+str(j)+'.zarr')
 
-    pda = da.from_array(filt0)
-    da.to_zarr(pda,'/scratch/schorb/HH_platy/Platy-12601_'+str(j)+'.zarr')
+    # return filt0
 
-    return j
-    
-         
 
-# =============================================================================
-# Process projections
-# =============================================================================
-    
-#do phase retrieval
-# time1 = time.time()
-# with closing(Pool(cp_count, initializer = init)) as pool:    
-#     pool.map(read_flat, np.arange(Pro.N_files))
-# #print('time for ff+shifts: ', time.time()-time1)
 
-# proj = tonumpyarray(proj.shared_array_base, proj.shape, proj.dtype)
 
-#res = client.map(read_flat,range(10),Pro=Pro)
+
+
+
+
+
+#%%
+
+
+# =============================
+
+
+# RUN  SCRIPT
+
+# =============================
+
+
+images, flats = init_paths(data_name, folder, distances)
+
+im_shape = (ROI[3]-ROI[1], ROI[2]-ROI[0])
+
+shape_ff = (N_distances, len(flats[0]), im_shape[0], im_shape[1])
+ff_shared = F(shape = shape_ff, dtype = 'd')
+
+
+#read ff-files to memory
+
+ff = np.zeros(shape_ff)
+
+for i in range(N_distances):
+    for j,fname in enumerate(flats[i]):
+        ff[i][j]=imread(fname)[ROI[1]:ROI[3], ROI[0]:ROI[2]]
+        
+
+
+#calculate ff-related constants
+ROI_ff = (ff.shape[3]//4, ff.shape[2]//4,3 * ff.shape[3]//4, 3 * ff.shape[2]//4)    # make ROI for further flatfield and shift corrections, same logic as for normal ROI
+ff_con = np.zeros(N_distances, 'object')                                                # array of classes to store flatfield-related constants
+for i in np.arange(N_distances):    
+    ff_con[i] = SSIM_const(ff[i][:,ROI_ff[1]:ROI_ff[3], 
+                                   ROI_ff[0]:ROI_ff[2]].transpose(1,2,0))
 
